0ab406c2bd8a12b24a835a48a29a173600b2ae80
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2026-01-12 23:21:45 +0100

    feat: configure global settings for database and Redis modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './db/db.module';
+import { RedisConfigModule } from './redis/redis.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    DatabaseModule,
+    RedisConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
