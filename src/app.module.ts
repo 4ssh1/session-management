77ec6873c6c2242f17b718058ff6da0740066116
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2026-01-13 23:37:15 +0100

    feat: implement authentication module with controllers, services, DTOs, and decorators

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SessionModule } from './session/session.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { SessionModule } from './session/session.module';
     RedisConfigModule,
     UsersModule,
     SessionModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
